14893d240650a825917a7bb14691859cbb859102
JonTheMathMan <suspect@example.com>
2018-08-12 13:33:46 -0700

    make the frontend of the repeat interval work

--- dropdown.js
+++ dropdown.js
@@ -5,17 +5,21 @@ function convertToDropdownElement(givenElement, options) {
 		selectedField.open = !selectedField.open;
 		if (selectedField.open) {
 			for(var i=0; i<options.length; i++){
-				var optionField = getDropdownField(options[i]);
+				let optionField = getDropdownField(options[i]);
 				optionField.onclick = function() {
 					selectedField.textContent = optionField.textContent;
-					for (var i2=1; i2<givenElement.children.length; i2++) {
+					if (givenElement.id === "repeatIntervalType") {
+						toggleValueFieldEnabled(optionField.textContent);
+					}
+					selectedField.open = !selectedField.open;
+					for (var i2=givenElement.children.length-1; i2>0; i2--) {
 						givenElement.removeChild(givenElement.children[i2]);
 					}
 				};
 				givenElement.appendChild(optionField);
 			}
 		} else {
-			for (var i2=1; i2<givenElement.children.length; i2++) {
+			for (var i2=givenElement.children.length-1; i2>0; i2--) {
 				givenElement.removeChild(givenElement.children[i2]);
 			}
 		}
@@ -33,11 +37,31 @@ function getDropdownField(fieldValue) {
 	return dropdownField;
 }
 
+function toggleValueFieldEnabled(intervalOption) {
+	var intervalValueField = document.getElementById("repeatIntervalValue");
+
+	if (["daily", "weekly - every same day of the week", "monthly - every Nth day of the month"].includes(intervalOption)){
+		intervalValueField.value = "";
+		intervalValueField.disabled = true;
+	}else{
+		intervalValueField.disabled = false;
+	}
+}
+
 function getDropdowns() {
 	var dropdowns = document.getElementsByClassName("eventDropdown");
 	for(var i=0; i<dropdowns.length; i++) {
 		switch(dropdowns[i].id) {
 			case "repeatIntervalType":
+				convertToDropdownElement(dropdowns[i], [
+					"daily",
+					"weekly - every same day of the week",
+					"monthly - every Nth day of the month",
+					"interval - every N days",
+					"interval - every N weeks on the same day of the week",
+					"interval - every N Months on the same day of the month"
+					]);
+				break;
 			case "accountName":
 			case "categoryTag":
 			default:

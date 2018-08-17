64f2b94a83c271325ad40ddbc2479eaa2ae01698
JonTheMathMan <suspect@example.com>
2018-08-17 00:46:09 -0700

    add eventID field and don't use empty field values

--- eventMaker.js
+++ eventMaker.js
@@ -1,8 +1,11 @@
-function createCalendarEvent() {
+function createCalendarEvent(submit) {
 	var eventFields = document.getElementsByClassName("createCalendarEvent");
 	var eventEntry = {};
+	eventEntry["eventID"] = Date.now();
+	var emptyValues = ["select an option", "no dropdown options", ""]
 	for(var i=0;i<eventFields.length;i++) {
-		eventEntry[eventFields[i].id] = eventFields[i].textContent;
+		eventEntry[eventFields[i].id] = emptyValues.includes(eventFields[i].textContent) ? undefined : eventFields[i].textContent;
+		eventEntry[eventFields[i].id] = emptyValues.includes(eventFields[i].value) ? undefined : eventFields[i].value;
 	}
 	console.log(JSON.stringify(eventEntry));
 }

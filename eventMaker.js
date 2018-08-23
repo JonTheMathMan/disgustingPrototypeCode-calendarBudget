c646761257b001a90f51b5eda417eb5fdbd4a518
JonTheMathMan <suspect@example.com>
2018-08-22 23:03:46 -0700

    save file with <a><button> combo

--- eventMaker.js
+++ eventMaker.js
@@ -1,4 +1,4 @@
-function createCalendarEvent(submit) {
+function createCalendarEvent() {
 	var eventFields = document.getElementsByClassName("createCalendarEvent");
 	var eventEntry = {};
 	eventEntry["eventID"] = Date.now();
@@ -7,5 +7,8 @@ function createCalendarEvent(submit) {
 		eventEntry[eventFields[i].id] = emptyValues.includes(eventFields[i].textContent) ? undefined : eventFields[i].textContent;
 		eventEntry[eventFields[i].id] = emptyValues.includes(eventFields[i].value) ? undefined : eventFields[i].value;
 	}
-	console.log(JSON.stringify(eventEntry));
+	alert("Calendar Events Budget:\n\nSaving file to browser downloads folder.\nIf you are on a phone,\n\t it is reccomended that the downloads folder be set to a path on the SD card in the browser settings.");
+	var a = document.getElementById("saveFile");
+	a.download    = "budgetEvents.json";
+	a.href        = URL.createObjectURL(new Blob([JSON.stringify(eventEntry)], {type: "application/json"}));
 }

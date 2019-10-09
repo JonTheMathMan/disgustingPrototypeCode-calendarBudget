ca6a35178181be197fbce3cd3a3dd7b36c831e82
JonTheMathMan <suspect@example.com>
2019-10-09 02:41:26 -0700

    Refactor/Rewrite calendar logic

--- pages/calendar/calendar.js
+++ pages/calendar/calendar.js
@@ -14,94 +14,155 @@ function getMonthLength(monthInt, fullYearInt){
 	return 31;	
 }
 
-var currentDate = new Date();
-var viewMonth = new Date();
-viewMonth.setDate(1);
-document.getElementById("viewMonthName").textContent = viewMonth.toDateString().slice(3,8) + viewMonth.getFullYear();
-showDays();
-function prevMonth() {
-	viewMonth.setMonth(viewMonth.getMonth()-1);
-	document.getElementById("viewMonthName").textContent = viewMonth.toDateString().slice(3,8) + viewMonth.getFullYear();
-	showDays();
-}
-function nextMonth() {
-	viewMonth.setMonth(viewMonth.getMonth()+1);
-	document.getElementById("viewMonthName").textContent = viewMonth.toDateString().slice(3,8) + viewMonth.getFullYear();
-	showDays();
-}
-function showDays(){
-	var daysOfMonthContainer = document.getElementById("daysOfMonthContainer");
-	for (var expectedChildren = 0; expectedChildren < 2; expectedChildren++) {
-		if (daysOfMonthContainer.firstElementChild !== null) {
-			daysOfMonthContainer.removeChild(daysOfMonthContainer.firstElementChild);
-		}
-	}
+// day of the week column headers
+var weekDayNames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
 
-	// container divs
-	var weekDayHeadersContainer = document.createElement("div");
-	var dayBoxesContainer = document.createElement("div");
-	var dayBoxesAbsolutePositionWrapper = document.createElement("div");
+var weekDayNamesShort = [
+	"Sun",
+	"Mon",
+	"Tues",
+	"Wed",
+	"Thurs",
+	"Fri",
+	"Sat"
+];
 
+function getNewCalendarView(thisElement, fullSize=true) {
 
-	// styling - div position variables
-	var dayOftheWeek = viewMonth.getDay();
-	var weekShift;
+	var currentDate = new Date();
+	var viewMonth = new Date();
 	var cyclePeriod = 101;
 	var marginSide = 10;
+	if (!fullSize) {
+		cyclePeriod = 41;
+	}
 
-	// container divs properties
-	weekDayHeadersContainer.id = "weekDayHeaders";
-	weekDayHeadersContainer.style.height = 10;
-	dayBoxesContainer.id = "dayBoxesContainer";
-	dayBoxesAbsolutePositionWrapper.style.position = "absolute";
+	if (!fullSize) {
+		weekDayNames = weekDayNamesShort;
+	}
+	
+	// set date to the first of the month
+	viewMonth.setDate(1);
 
-	// day of the week column headers
-	var weekDayNames = [
-		"Sunday",
-		"Monday",
-		"Tuesday",
-		"Wednesday",
-		"Thursday",
-		"Friday",
-		"Saturday"
-	];
-	for (var weekDayIndex in weekDayNames){
+	// show which month is in view
+	var viewMonthName = document.createElement("h5");
+	thisElement.appendChild(viewMonthName);
+	viewMonthName.textContent = viewMonth.toDateString().slice(3,8) + viewMonth.getFullYear();
+	
+	// view previous month
+	var prevMonth = function() {
+		viewMonth.setMonth(viewMonth.getMonth()-1);
+		viewMonthName.textContent = viewMonth.toDateString().slice(3,8) + viewMonth.getFullYear();
+		showDays();
+	};
+	var prevMonthButton = document.createElement("button");
+	prevMonthButton.onclick = prevMonth;
+	prevMonthButton.textContent = "Previous Month";
+	thisElement.appendChild(prevMonthButton);
+
+	// view next month
+	var nextMonth = function() {
+		viewMonth.setMonth(viewMonth.getMonth()+1);
+		viewMonthName.textContent = viewMonth.toDateString().slice(3,8) + viewMonth.getFullYear();
+		showDays();
+	};
+	var nextMonthButton = document.createElement("button");
+	nextMonthButton.onclick = nextMonth;
+	nextMonthButton.textContent = "Next Month";
+	thisElement.appendChild(nextMonthButton);
+
+	// get week day Header Element For Calendar
+	var getWeekDayHeader = function(weekDayIndex, weekDayNames){
 		var weekDayHeader = document.createElement("div");
-		weekDayHeader.style.position = "absolute";
-		weekDayHeader.style.left = cyclePeriod * weekDayIndex + marginSide;
-		weekDayHeader.style.width = cyclePeriod;
-		weekDayHeader.style.textAlign = "center";
-		weekDayHeader.textContent = weekDayNames[weekDayIndex];
-		weekDayHeadersContainer.appendChild(weekDayHeader);
-	}
-	daysOfMonthContainer.appendChild(weekDayHeadersContainer);
+			weekDayHeader.style.position = "absolute";
+			weekDayHeader.style.left = cyclePeriod * weekDayIndex + marginSide;
+			weekDayHeader.style.width = cyclePeriod;
+			weekDayHeader.style.textAlign = "center";
+			weekDayHeader.textContent = weekDayNames[weekDayIndex];
+			return weekDayHeader;
+	};
 
-	// dayBoxes
-	for(var i=0; i<getMonthLength(viewMonth.getMonth(), viewMonth.getFullYear()); i++) {
-		weekShift = i+dayOftheWeek;
-		var dayBox = document.createElement("div");
-		dayBox.className = "dayBox";
-		dayBox.style.left = cyclePeriod * (weekShift % 7) + marginSide;
-		dayBox.style.top = cyclePeriod * Math.floor(weekShift/7) + marginSide;
-		dayBoxesContainer.style.height = cyclePeriod * Math.floor(weekShift/7) + cyclePeriod + 2 * marginSide;
-		dayBox.textContent = i+1;
-		dayBox.dateDayNumber = i+1;
-		dayBox.getDate = function(){return this.dateDayNumber;};
-		dayBox.onmouseover = function(){
-			this.textContent += " Add Event?";
-		};
-		dayBox.onmouseleave = function(){
-			this.textContent = this.getDate();
-		};
-		dayBox.onclick = function(){
-			viewMonth.setDate(this.getDate());
-			document.getElementById("date").value = viewMonth.toDateString();
-			document.getElementById("calendarPage").hidden = true;
-			document.getElementById("newEventPage").hidden = false;
-			selectedPage = "newEventPage";
-		};
-		dayBoxesAbsolutePositionWrapper.appendChild(dayBox);
-	}
-	dayBoxesContainer.appendChild(dayBoxesAbsolutePositionWrapper);
-	daysOfMonthContainer.appendChild(dayBoxesContainer);
+	var getWeekDayHeadersContainer = function() {
+		var weekDayHeadersContainer = document.createElement("div");
+		// weekDayHeadersContainer.id = "weekDayHeaders";
+		weekDayHeadersContainer.style.height = 10;
+		for (var weekDayIndex in weekDayNames){
+			var weekDayHeader = getWeekDayHeader(weekDayIndex, weekDayNames)
+			weekDayHeadersContainer.appendChild(weekDayHeader);
+		}
+		return weekDayHeadersContainer;
+	};
+
+	thisElement.appendChild(getWeekDayHeadersContainer());
+
+	// use chosen date in the create event window
+	var createEventWithThisDate = function(){
+		viewMonth.setDate(this.getDate());
+		document.getElementById("date").value = viewMonth.toDateString();
+		document.getElementById("calendarPage").hidden = true;
+		document.getElementById("newEventPage").hidden = false;
+		selectedPage = "newEventPage";
+	};
+
+	// set the selected date for date input
+	var setSelectedDate = function() {
+		viewMonth.setDate(this.getDate());
+		theElement.SelectedDate = viewMonth;
+	};
+
+	// update the calendar with the month's day box divs
+	var showDays = function(){
+		var daysOfMonthContainer = thisElement;
+		// five items appended to this element total which is 4 in 0 based indexing
+		var expectedDayBoxContainerIndex = 4;
+		if (daysOfMonthContainer.children[expectedDayBoxContainerIndex] !== undefined) {
+			daysOfMonthContainer.removeChild(daysOfMonthContainer.children[expectedDayBoxContainerIndex]);
+		}
+
+		// container divs
+		var dayBoxesContainer = document.createElement("div");
+		var dayBoxesAbsolutePositionWrapper = document.createElement("div");
+
+		// container divs properties
+		// dayBoxesContainer.id = "dayBoxesContainer";
+		dayBoxesAbsolutePositionWrapper.style.position = "absolute";
+
+		// styling - div position variables
+		var dayOftheWeek = viewMonth.getDay();
+		var weekShift;
+
+		// dayBoxes
+		for(var i=0; i<getMonthLength(viewMonth.getMonth(), viewMonth.getFullYear()); i++) {
+			weekShift = i+dayOftheWeek;
+			var dayBox = document.createElement("div");
+			dayBox.className = "dayBox";
+			dayBox.style.left = cyclePeriod * (weekShift % 7) + marginSide;
+			dayBox.style.top = cyclePeriod * Math.floor(weekShift/7) + marginSide;
+			dayBox.style.height = cyclePeriod - 1;
+			dayBox.style.width = cyclePeriod - 1;
+			dayBoxesContainer.style.height = cyclePeriod * Math.floor(weekShift/7) + cyclePeriod + 2 * marginSide;
+			dayBox.textContent = i+1;
+			dayBox.dateDayNumber = i+1;
+			dayBox.getDate = function(){return this.dateDayNumber;};
+			if (fullSize === true) {
+				dayBox.onclick = createEventWithThisDate;
+			}else {
+				dayBox.onclick = setSelectedDate;
+			}
+			dayBoxesAbsolutePositionWrapper.appendChild(dayBox);
+		}
+		dayBoxesContainer.appendChild(dayBoxesAbsolutePositionWrapper);
+		daysOfMonthContainer.appendChild(dayBoxesContainer);
+	};
+	showDays();
 }
+
+getNewCalendarView(document.getElementById("daysOfMonthContainer"));
--- pages/calendar/calendarHtml.js
+++ pages/calendar/calendarHtml.js
@@ -1,5 +1,1 @@
-var calendarHtml = `<button onclick="prevMonth()">backward</button>
-    <button onclick="nextMonth()">forward</button>
-    <h5 id="viewMonthName"></h5>
-    <div id="weekdayNames"></div>
-    <div id="daysOfMonthContainer" style="background-color:lightgreen;position:relative;"></div>`;
+var calendarHtml = `<div id="daysOfMonthContainer" style="background-color:lightgreen;position:relative;"></div>`;
